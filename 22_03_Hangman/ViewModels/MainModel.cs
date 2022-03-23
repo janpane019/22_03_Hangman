49a839be8e30e844b7b36759d7fb179745aa12ea
Jan Pánek <u19.418@pslib.cloud>
2022-03-23 10:09:04 +0100

    Přidejte soubory projektu.

---
+++ 22_03_Hangman/ViewModels/MainModel.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _22_03_Hangman.ViewModels
+{
+    internal class MainModel : INotifyPropertyChanged
+    {
+        public MainModel()
+        {
+            PressCommand = new RelayCommand<string>(
+                (value) =>
+                {
+                    // Add character to list and update CanExecute
+                    UsedChars.Add(value);
+                    PressCommand.RaiseCanExecuteChanged();
+
+                    // Check if character is in OriginalText
+                    if (!OriginalText.ToUpper().Contains(value.ToUpper()))
+                    {
+                        WrongGuesses++;
+                    }
+
+                    if(GuessText == OriginalText)
+                    {
+                        WinnerText = "You won!";
+                        CanEdit = true;
+                        PressCommand.RaiseCanExecuteChanged();
+                        StartCommand.RaiseCanExecuteChanged();
+                    }
+
+                    // Update property
+                    NotifyPropertyChanged("GuessText");
+                },
+                (value) =>
+                {
+                    // Check if character is in list and if game started
+                    return !UsedChars.Contains(value) && !CanEdit;
+                }
+            );
+            StartCommand = new RelayCommand(
+                () =>
+                {
+                    CanEdit = false;
+                    WinnerText = "";
+                    UsedChars = new ObservableCollection<string>();
+                    WrongGuesses = 0;
+                    NotifyPropertyChanged("GuessText");
+                    PressCommand.RaiseCanExecuteChanged();
+                    StartCommand.RaiseCanExecuteChanged();
+                },
+                () =>
+                {
+                    return CanEdit;
+                }
+            );
+            UsedChars = new ObservableCollection<string>();
+            wrongGuesses = 0;
+            originalText = "Lorem Ipsum";
+            canEdit = true;
+            winnerText = "";
+        }
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public RelayCommand<string> PressCommand { get; set; }
+        public RelayCommand StartCommand { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private bool canEdit;
+        public bool CanEdit
+        {
+            get
+            {
+                return canEdit;
+            }
+            set
+            {
+                canEdit = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string originalText;
+        public string OriginalText 
+        { 
+            get 
+            {
+                return originalText;
+            }
+            set
+            {
+                originalText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string GuessText
+        {
+            get
+            {
+                string result = "";
+                foreach(char character in OriginalText)
+                {
+                    result += UsedChars.Contains(character.ToString().ToUpper()) || character == ' ' ? character : "-";
+                }
+                return result;
+            }
+        }
+
+        private int wrongGuesses;
+        public int WrongGuesses
+        {
+            get
+            {
+                return wrongGuesses;
+            }
+            set
+            {
+                wrongGuesses = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string winnerText;
+        public string WinnerText
+        {
+            get
+            {
+                return winnerText;
+            }
+            set
+            {
+                winnerText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<string> usedChars;
+        public ObservableCollection<string> UsedChars
+        {
+            get
+            {
+                return usedChars;
+            }
+            set
+            {
+                usedChars = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
---
+++ 22_03_Hangman/ViewModels/RelayCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace _22_03_Hangman.ViewModels
+{
+    internal class RelayCommand<T> : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action<T> _execute;
+        private Func<T, bool> _canExecute;
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (_canExecute == null) return true;
+            if (parameter == default) return _canExecute(default);
+            return _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+    internal class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action _execute;
+        private Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
