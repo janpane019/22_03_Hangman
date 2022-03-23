c9931fb2ccc9b2ba505dc79fcad8784915bec75d
Jan Pánek <u19.418@pslib.cloud>
2022-03-23 10:41:45 +0100

    add hankmen

---
+++ 22_03_Hangman/ViewModels/VisibleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace _22_03_Hangman.ViewModels
+{
+    internal class VisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var test = (value.ToString() == parameter.ToString());
+            return (int.Parse(value.ToString()) >= int.Parse(parameter.ToString())) ? "Visible" : "Hidden";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
